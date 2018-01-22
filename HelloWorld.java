162b5be3d8d2b9067ea7446d08beb7c9cd503c09
Rajesh Chanda <rajeshchanda1510@yahoo.com>
2018-01-22 14:08:13 +0530

    Decremented the counter

--- HelloWorld.java
+++ HelloWorld.java
@@ -20,7 +20,7 @@ public class HelloWorld {
 
    	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	for(int i=1;i<=10;i++)
+	for(int i=1;i<10;i++)
 	{
 	System.out.println("Hello World ......"+ i );
 	}
